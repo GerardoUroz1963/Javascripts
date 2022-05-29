a8ee1d0f64b0a124154891b2c1c47fccd2365969
Gerardo A. Uroz <elchaca2007@gmail.com>
2022-05-29 18:06:45 -0300

    Variables Fin

--- Clase3/valores.js
+++ Clase3/valores.js
@@ -1,8 +1,8 @@
 40
-print(typeof(40))
-print(typeof("pedro"))
-print(typeof (null))
-print(typeof (undefined))
-typeof true
-typeof [1,2,3]
-typeof {nombre: "Diego"}
+print(typeof(40));
+print(typeof("pedro"));
+print(typeof (null));
+print(typeof (undefined));
+typeof true;
+typeof [1,2,3];
+typeof {nombre: "Diego"};
---
+++ Clase3/variables.js
@@ -0,0 +1,17 @@
+var nombre = "Oscar";
+
+//declarar
+
+var edad;
+
+//Inicializar 
+edad = 59;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+print (persona)

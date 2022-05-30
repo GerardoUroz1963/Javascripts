f9c00d702e4700216cbce160a04192e89b54ed73
Gerardo A. Uroz <elchaca2007@gmail.com>
2022-05-29 21:11:34 -0300

    Scope, entonro o alcance de las Variables

---
+++ Clase3/scope.js
@@ -0,0 +1,14 @@
+function fun(){
+    var apellido = "Uroz";
+    return  apellido + ", "+ nombre;
+}
+var nombre = "Gerardo Antonio"
+console.log(fun())
+
+var miNombre = "Gerardo Antonio";
+function nombre(){
+    var miApellido = "Uroz"
+    console.log(miNombre + " " + miApellido)
+}
+
+nombre();
